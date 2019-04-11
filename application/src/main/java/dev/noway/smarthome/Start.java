8668f3cd22b5ff1690a878c76552ccaa3377b08b
noway0032@gmail.com <noway0032@gmail.com>
2019-04-11 14:25:22 +0200

    Startup repair

--- application/src/main/java/dev/noway/smarthome/Start.java
+++ application/src/main/java/dev/noway/smarthome/Start.java
@@ -4,7 +4,6 @@ import dev.noway.smarthome.mqtt.MqttConnection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Start {
     public static void main(String[] args) throws Exception {
